84b7b0df11a7d18b5c74e4f9b43bebf0275545e6
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-04-21 17:50:24 +0300

    Урок 51: [RoR] Шаблоны, ERB, route helpers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,4 +21,12 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @question.destroy
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
 end
