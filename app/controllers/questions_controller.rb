bfe09da6541d5e1f41fd5aeff29fc4e20602bdbf
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-05-08 21:22:44 +0300

    add action hide

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update show destroy edit]
+  before_action :set_question, only: %i[update show destroy edit hide]
+
   def create
     question = Question.create(question_params)
 
@@ -32,12 +33,21 @@ class QuestionsController < ApplicationController
   def edit
   end
 
+  def hide
+    @question.hidden = !@question.hidden
+    # debugger
+    @question.update(hidden: @question.hidden)
+    # @question.update(question_params)
+  end
+
   private
   def question_params
-    params.require(:question).permit(:body, :user_id)
+    debugger
+    params.require(:question).permit(:body, :user_id, :hidden)
   end
 
   def set_question
     @question = Question.find(params[:id])
+    # debugger
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
+  put '/questions/:id/hide', to: 'questions#hide', as: 'hide_question'
   resources :questions
 end
