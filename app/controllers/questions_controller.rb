8b90ab929e78769c2b0f19e169e4fdf0f7d197dd
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-05-25 21:47:01 +0300

    Урок 53: [RoR] Паршлы, flash, хэлперы

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,19 +4,19 @@ class QuestionsController < ApplicationController
   def create
     question = Question.create(question_params)
 
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: 'Вопрос создан!'
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Вопрос обновлен!'
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удален!'
   end
 
   def show
