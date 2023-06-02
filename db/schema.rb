40310670faee0b9b64ac29e442dcd0598ecce09f
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-06-02 13:33:19 +0300

    L54-3 add authenticate to project & fix typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_171358) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_095026) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_171358) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
