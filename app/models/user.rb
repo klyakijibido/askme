01e922f538fecb0e2277fb7f9561e8f450735ecd
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-06-04 17:27:59 +0300

    55-v2. Add callback downcase_nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  before_validation :downcase_nickname
+
   validates :email, presence: true, uniqueness: true
+
+  private
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
