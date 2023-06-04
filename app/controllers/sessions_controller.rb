93bc5915a046026276782f35ec2852a2a6757e13
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-06-04 20:14:17 +0300

    Add autologin after registration (Homework 54-1)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user_params = params.require(:session)
 
     user = User.find_by(email: user_params[:email])&.authenticate(user_params[:password])
- 
+
     if user.present?
       session[:user_id] = user.id
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def new
-    session[:current_time] = Time.now
     @user = User.new
   end
 
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
-    User.create(user_params)
+    user = User.create(user_params)
+    session[:user_id] = user.id
 
     redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
   end
