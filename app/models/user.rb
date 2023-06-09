36b893ca163a3c067b145860ebe8cb07135049af
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-06-09 17:34:53 +0300

    55. Add validates regexp email(homework 55-1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :nickname, format: { with: /\A[a-z0-9_]{3,40}\z/ }, presence: true, uniqueness: true
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A[a-z0-9._%+-]+@[a-z0-9-]+.+.[a-z]{2,4}\z/ },  presence: true, uniqueness: true
 
   def downcase_nickname
     nickname.downcase!
