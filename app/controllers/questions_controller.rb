510ec550cb84915593b2b6d20ecbb522e420ce25
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-07-08 19:34:54 +0300

    56. Add edit and delete questions only same user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update show destroy edit hide]
+  before_action :set_question_for_current_user, only: %i[update destroy edit hide]
 
   def create
     @question = Question.new(question_params)
@@ -38,6 +38,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def index
@@ -61,7 +62,7 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:body, :user_id)
   end
 
-  def set_question
-    @question = Question.find(params[:id])
+  def set_question_for_current_user
+    @question = current_user.questions.find(params[:id])
   end
 end
