e03b23c6f2193f2541e707482da8a05c75a55502
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-07-18 22:32:57 +0300

    Changed 'id' to 'nickname' in the user path(homework 57-2)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -52,7 +52,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
+    @user = User.find_by!(nickname: params[:nickname])
     @question = Question.new(user: @user)
   end
 
